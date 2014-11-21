2a1ade03313912a264b02a70a5e809bdbd604dec
Francis Stephens <francisstephens@gmail.com>
2014-11-21 17:34:48 +0000

    Added sequential and parrallel empty{} tests
    The sequential test puts 1000 empty{} entities into the datastore. The
    Parallel test puts 1000 empty{} entities into the datastore using a
    goroutine for each one.
    
    The results are that sequential Puts run at about 30-40ms with outliers.
    Putting 1000 entities takes about 40 seconds.
    
    Parallel puts take 200-600ms with the whole request taking slightly
    longer than the slowest Put, i.e. a bit more than 600ms.

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Performance test 1. Low Kind populations
 
 When there are few entities of a particular kind both the entity writes and index writes will end up on the same big-table tablet (same physical machine). Because most benchmarks will start with an empty datastore if we do not control this factor our tests won't be representative. So we need to establish a minumum number of entities of a given kind that are needed before this effect is lost.
 
+The result is that this effect is very modest. We can see an initially slow set of puts for the first 100 entities. As long as a performance test creates more than 1000 this effect shouldn't be too pronounced. Each put ranges from just under 200 ms for the first 100 puts settling down to 30-40ms with occasional outliers around 100-200ms.
+
 Performance test 2. Independent Entity Writes
 
 At what rate can we expect to write entities which are completely independent. Does the number of indexed properties affect this? By how much?
--- common.go
+++ common.go
@@ -19,17 +19,76 @@ func (e *empty) kind() string {
 }
 
 func init() {
-	http.HandleFunc("/fewKinds", fewKindsHandler)
+	http.HandleFunc("/fewKindsSequential", fewKindsSequentialHandler)
+	http.HandleFunc("/fewKindsParallel", fewKindsParallelHandler)
+	http.HandleFunc("/delEmpty", delEmptyHandler)
 }
 
-func fewKindsHandler(w http.ResponseWriter, r *http.Request) {
+const fewKindsCount = 1000
+const delLimit = 500
+
+func fewKindsSequentialHandler(w http.ResponseWriter, r *http.Request) {
 	cxt := appengine.NewContext(r)
-	for i := 0; i < 1000; i++ {
-		start := time.Now()
-		e := &empty{}
-		k := datastore.NewIncompleteKey(cxt, e.kind(), nil)
-		datastore.Put(cxt, k, e)
-		total := time.Now().Sub(start)
-		cxt.Infof("Few Kinds: %v", total)
+	outerStart := time.Now()
+	for i := 0; i < fewKindsCount; i++ {
+		if err := putEmpty(cxt, i); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+	}
+	outerTotal := time.Now().Sub(outerStart)
+	cxt.Infof("Few Kinds %d Puts: %v", fewKindsCount, outerTotal)
+}
+
+func fewKindsParallelHandler(w http.ResponseWriter, r *http.Request) {
+	cxt := appengine.NewContext(r)
+	outerStart := time.Now()
+	complete := make(chan bool, fewKindsCount)
+	for i := 0; i < fewKindsCount; i++ {
+		go func(count int) {
+			if err := putEmpty(cxt, count); err != nil {
+				cxt.Infof("%s", err.Error())
+			}
+			complete <- true
+		}(i)
+	}
+	for i := 0; i < fewKindsCount; i++ {
+		<-complete
+	}
+	outerTotal := time.Now().Sub(outerStart)
+	cxt.Infof("Few Kinds %d Puts: %v", fewKindsCount, outerTotal)
+}
+
+func putEmpty(cxt appengine.Context, count int) error {
+	start := time.Now()
+	e := &empty{}
+	k := datastore.NewIncompleteKey(cxt, e.kind(), nil)
+	if _, err := datastore.Put(cxt, k, e); err != nil {
+		return err
+	}
+	total := time.Now().Sub(start)
+	cxt.Infof("%d Few Kinds Single Put: %v", count, total)
+	return nil
+}
+
+func delEmptyHandler(w http.ResponseWriter, r *http.Request) {
+	cxt := appengine.NewContext(r)
+	e := &empty{}
+	for i := 0; i < 4; i++ {
+		q := datastore.NewQuery(e.kind()).KeysOnly().Limit(delLimit)
+		keys, err := q.GetAll(cxt, make([]*empty, 0))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+		cxt.Infof("Deleting Empties: %d", len(keys))
+		if err := datastore.DeleteMulti(cxt, keys); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+		cxt.Infof("Sucessfully Deleted Empties: %d", len(keys))
 	}
 }
