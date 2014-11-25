7905d9d1599cc20469efbe3e417e9caf78d6b635
Francis Stephens <francisstephens@gmail.com>
2014-11-25 15:17:46 +0000

    Generalised puts and deletes
    The empty entity tests now just use the common functions to put and
    delete empty entities in the datastore.

--- common.go
+++ common.go
@@ -7,8 +7,14 @@ import (
 	"time"
 )
 
+const putCount = 200
+
 type kinder interface {
 	kind() string
+}
+
+type kinderBuilder interface {
+	build() kinder
 }
 
 func init() {
@@ -26,4 +32,58 @@ func putKinder(cxt appengine.Context, entity kinder, count int) error {
 	total := time.Now().Sub(start)
 	cxt.Infof("%d %s Single Put: %v", count, entity.kind(), total)
 	return nil
+}
+
+func putKinderSequential(w http.ResponseWriter, r *http.Request, kBuilder kinderBuilder) {
+	outerStart := time.Now()
+	cxt := appengine.NewContext(r)
+	for i := 0; i < putCount; i++ {
+		entity := kBuilder.build()
+		if err := putKinder(cxt, entity, i); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+	}
+	outerTotal := time.Now().Sub(outerStart)
+	cxt.Infof("Few Kinds %d Puts: %v", putCount, outerTotal)
+}
+
+func putKinderParallel(w http.ResponseWriter, r *http.Request, kBuilder kinderBuilder) {
+	outerStart := time.Now()
+	cxt := appengine.NewContext(r)
+	complete := make(chan bool, putCount)
+	for i := 0; i < putCount; i++ {
+		entity := kBuilder.build()
+		go func(count int) {
+			if err := putKinder(cxt, entity, count); err != nil {
+				cxt.Infof("%s", err.Error())
+			}
+			complete <- true
+		}(i)
+	}
+	outerTotal := time.Now().Sub(outerStart)
+	cxt.Infof("Few Kinds %d Puts: %v", putCount, outerTotal)
+}
+
+func delKind(w http.ResponseWriter, r *http.Request, kind string) {
+	cxt := appengine.NewContext(r)
+	q := datastore.NewQuery(kind).KeysOnly().Limit(500)
+	var keys []*datastore.Key
+	var err error
+	for keys == nil || len(keys) > 0 {
+		keys, err = q.GetAll(cxt, make([]*empty, 0))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+		cxt.Infof("Deleting %s: %d", kind, len(keys))
+		if err := datastore.DeleteMulti(cxt, keys); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			cxt.Infof("%s", err.Error())
+			return
+		}
+		cxt.Infof("Sucessfully Deleted %s: %d", kind, len(keys))
+	}
 }
--- empty.go
+++ empty.go
@@ -1,10 +1,7 @@
 package mercury
 
 import (
-	"appengine"
-	"appengine/datastore"
 	"net/http"
-	"time"
 )
 
 type empty struct {
@@ -14,59 +11,22 @@ func (e *empty) kind() string {
 	return "empty"
 }
 
-const emptyCount = 1000
+type emptyBuilder struct{}
+
+func (b *emptyBuilder) build() kinder {
+	return &empty{}
+}
 
 func emptySequentialHandler(w http.ResponseWriter, r *http.Request) {
-	cxt := appengine.NewContext(r)
-	outerStart := time.Now()
-	for i := 0; i < emptyCount; i++ {
-		if err := putKinder(cxt, &empty{}, i); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			cxt.Infof("%s", err.Error())
-			return
-		}
-	}
-	outerTotal := time.Now().Sub(outerStart)
-	cxt.Infof("Few Kinds %d Puts: %v", emptyCount, outerTotal)
+	b := &emptyBuilder{}
+	putKinderSequential(w, r, b)
 }
 
 func emptyParallelHandler(w http.ResponseWriter, r *http.Request) {
-	cxt := appengine.NewContext(r)
-	outerStart := time.Now()
-	complete := make(chan bool, emptyCount)
-	for i := 0; i < emptyCount; i++ {
-		go func(count int) {
-			if err := putKinder(cxt, &empty{}, count); err != nil {
-				cxt.Infof("%s", err.Error())
-			}
-			complete <- true
-		}(i)
-	}
-	for i := 0; i < emptyCount; i++ {
-		<-complete
-	}
-	outerTotal := time.Now().Sub(outerStart)
-	cxt.Infof("Few Kinds %d Puts: %v", emptyCount, outerTotal)
+	b := &emptyBuilder{}
+	putKinderParallel(w, r, b)
 }
 
 func delEmptyHandler(w http.ResponseWriter, r *http.Request) {
-	cxt := appengine.NewContext(r)
-	q := datastore.NewQuery((&empty{}).kind()).KeysOnly().Limit(500)
-	var keys []*datastore.Key
-	var err error
-	for keys == nil || len(keys) > 0 {
-		keys, err = q.GetAll(cxt, make([]*empty, 0))
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			cxt.Infof("%s", err.Error())
-			return
-		}
-		cxt.Infof("Deleting Empties: %d", len(keys))
-		if err := datastore.DeleteMulti(cxt, keys); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			cxt.Infof("%s", err.Error())
-			return
-		}
-		cxt.Infof("Sucessfully Deleted Empties: %d", len(keys))
-	}
+	delKind(w, r, (&empty{}).kind())
 }
